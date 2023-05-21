f87f47287e9bd6f86e4e1dd5129f11ab35f5bf57
Suraj omar <100358484+suraj5621@users.noreply.github.com>
2023-05-21 15:02:25 +0530

    Add files via upload

---
+++ Day 2 Challenge/By using warrenty/Service.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service{
+
+    List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p){
+        products.add(p);
+    }
+
+    public List<Product> getAllProducts(){
+        return products;
+    }
+
+  
+    
+    public Product getProduct(int warranty){
+        for(Product p : products){
+            if(p.getWarranty()==warranty)
+                return p;
+        }
+
+        return null;
+    }
+
+    
+
+    public List<Product> getProductWithText(String text) {
+        String str = text.toLowerCase();
+        List<Product> prods = new ArrayList<>();
+
+        for(Product p : products){
+            String name = p.getName().toLowerCase();
+            String type = p.getType().toLowerCase();
+            String place = p.getPlace().toLowerCase();
+            if(name.contains(str) || type.contains(str) || place.contains(str))
+                prods.add(p);
+        }
+        return prods;
+
+    }
+}
